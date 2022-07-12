434393956a8c10529e22c4e1a2742e7056c04c83
Jeniffer1976 <21025290@myrp.edu.sg>
2022-07-12 22:23:25 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id21025290/demodatabasecrud/MainActivity.java
@@ -0,0 +1,117 @@
+package sg.edu.rp.c346.id21025290.demodatabasecrud;
+
+import androidx.annotation.ColorInt;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnAdd, btnEdit, btnRetrieve;
+    EditText etContent;
+    ListView lv;
+    ArrayList <Note> al;
+    ArrayAdapter <Note> aa;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnAdd = findViewById(R.id.btnAdd);
+        btnEdit = findViewById(R.id.btnEdit);
+        btnRetrieve = findViewById(R.id.btnRetrieve);
+        etContent = findViewById(R.id.etContent);
+        lv = findViewById(R.id.lv);
+
+        al = new ArrayList<Note>();
+        aa = new ArrayAdapter<Note>(this,
+                android.R.layout.simple_list_item_1, al);
+        lv.setAdapter(aa);
+        Intent iTarget = new Intent(MainActivity.this,
+                EditActivity.class);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int
+                    position, long identity) {
+                Note data = al.get(position);
+                iTarget.putExtra("data", data);
+                for (int i = 0; i<lv.getChildCount(); i++){
+                    if(position == i){
+                        lv.getChildAt(i).setBackgroundColor(Color.parseColor("#E6E6E6"));
+                    } else {
+                        lv.getChildAt(i).setBackgroundColor(Color.parseColor("#FFFFFF"));
+
+                    }
+                }
+                view.setBackgroundColor(Color.parseColor("#E6E6E6"));
+
+            }
+        });
+
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data = etContent.getText().toString();
+                DBHelper dbh = new DBHelper(MainActivity.this);
+                long inserted_id = dbh.insertNote(data);
+
+                if (inserted_id != -1){
+                    al.clear();
+                    al.addAll(dbh.getAllNotes());
+                    aa.notifyDataSetChanged();
+                    Toast.makeText(MainActivity.this, "Insert successful",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        btnRetrieve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbh = new DBHelper(MainActivity.this);
+                al.clear();
+                String filterText = etContent.getText().toString().trim();
+                if(filterText.length() == 0) {
+                    al.addAll(dbh.getAllNotes());
+                }
+                else{
+                    al.addAll(dbh.getAllNotes(filterText));
+                }
+                aa.notifyDataSetChanged();
+
+            }
+
+        });
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(iTarget);
+            }
+        });
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        btnRetrieve.performClick();
+    }
+}
+
+
+
